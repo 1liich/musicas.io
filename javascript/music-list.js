4745c4c965702f149039d73af31f5d4af81ecfae
Alexandre Mendonça <114765222+1liich@users.noreply.github.com>
2025-02-11 18:43:55 -0400

    Add files via upload

---
+++ javascript/music-list.js
@@ -0,0 +1,92 @@
+let allMusic = [
+    {
+        name: "Happier Than Ever",
+        artist: "Billie Eilish",
+        img: "adventuretime",
+        src: "happierthanever"
+    },
+    {
+        name: "Lo-fi Beat - Arcane",
+        artist: "Samuel Kim",
+        img: "arcane",
+        link: "https://ia600107.us.archive.org/27/items/happierthaneverrr/happierthanever.mp3" // Música externa
+    },
+    {
+        name: "Lo-fi Beat - Blind In Love",
+        artist: "Lee",
+        img: "blindinlove",
+        src: "blindinlove"
+    },
+    {
+        name: "Lo-fi Beat - Blue Moon",
+        artist: "Eric Godlow",
+        img: "bluemoon",
+        src: "bluemoon"
+    },
+    {
+        name: "Lo-fi Beat - Close To You",
+        artist: "Lee",
+        img: "closetoyou",
+        src: "closetoyou"
+    },
+    {
+        name: "Lo-fi Beat - Dreaming of You",
+        artist: "GC",
+        img: "dreamingofyou",
+        src: "dreamingofyou"
+    },
+    {
+        name: "Lo-fi Beat - 90's Freestyle",
+        artist: "Yusei",
+        img: "freestyle",
+        src: "freestyle"
+    },
+    {
+        name: "Lo-fi Beat - Gravity Falls",
+        artist: "L.Dre",
+        img: "gravityfalls",
+        src: "gravityfalls"
+    },
+    {
+        name: "Lo-fi Beat - I Need a Girl",
+        artist: "Lee",
+        img: "ineedagirl",
+        src: "ineedagirl"
+    },
+    {
+        name: "Lo-fi Beat - Lovely",
+        artist: "Eric Godlow",
+        img: "lovely",
+        src: "lovely"
+    },
+    {
+        name: "Lo-fi Beat - Mortified",
+        artist: "L.Dre",
+        img: "mortified",
+        src: "mortified"
+    },
+    {
+        name: "Lo-fi Beat - Rainin Outside",
+        artist: "Akitō",
+        img: "rainin",
+        src: "raininoutside"
+    },
+    {
+        name: "Lo-fi Beat - Rickroll But It's Lofi",
+        artist: "KCFM",
+        img: "rickroll",
+        src: "rickroll"
+    },
+    {
+        name: "Lo-fi Beat - Sad Nights",
+        artist: "Eric Godlow",
+        img: "sad",
+        src: "sadnights"
+    },
+    {
+        name: "Lo-fi Beat - Steven Universe",
+        artist: "L.Dre",
+        img: "steven",
+        src: "stevenuniverse"
+    }
+]
