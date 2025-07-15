ee40238b93518d1bfede5609f5986fe75886b402
Alexandre Mendonça <114765222+1liich@users.noreply.github.com>
2025-07-14 22:53:23 -0400

    Update music-list.js

--- javascript/music-list.js
+++ javascript/music-list.js
@@ -30,10 +30,10 @@ let allMusic = [
         link: "https://ia601707.us.archive.org/11/items/kiisssmeref/lebanon%20hanover%20-%20kiss%20me%20until%20my%20lips%20fall%20off%20%28tradu%C3%A7%C3%A3o%E2%A7%B8legendado%29%20%5Bv_WPLnjyHAQ%5D.mp3"
     },
     {
-        name: "Lo-fi Beat - Dreaming of You",
-        artist: "GC",
-        img: "dreamingofyou",
-        src: "dreamingofyou"
+        name: "join me in death",
+        artist: "HIM",
+        img: "him",
+        src: "https://ia902902.us.archive.org/34/items/him-join-me-in-death-traducaolegendado-pd-qw-8-usu-io/him%20-%20join%20me%20in%20death%20%28tradu%C3%A7%C3%A3o%E2%A7%B8legendado%29%20%5BpdQw8-USuIo%5D.mp3"
     },
     {
         name: "Lo-fi Beat - 90's Freestyle",
